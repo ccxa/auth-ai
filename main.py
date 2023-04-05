d5ca5a998a45984b401ce49bead2fd191b793ec3
janel <janel.me@yahoo.com>
2023-04-05 20:32:52 +0330

    DEV-007: adding random string to begining of the incoming files, to prevent conflict

--- main.py
+++ main.py
@@ -2,22 +2,25 @@ import uvicorn
 from fastapi import FastAPI, File, UploadFile
 import shutil
 from face import compare_faces
+from utils import random_string
 
 # TODO: This path should be inside .env like file
 # TODO: This directory should automatically created when program starts
 storage_path = './temp/media/'
+random_string_length = 10
 
 app = FastAPI()
 
 
 @app.post("/get_file")
 async def get(image: UploadFile = File(...), video: UploadFile = File(...)):
+    prefix = random_string(random_string_length)
 
-    image_path = storage_path + image.filename
+    image_path = storage_path + prefix + image.filename
     with open(image_path, "wb") as buffer:
         shutil.copyfileobj(image.file, buffer)
 
-    video_path = storage_path + video.filename
+    video_path = storage_path + prefix + video.filename
     with open(video_path, "wb") as buffer:
         shutil.copyfileobj(video.file, buffer)
 
